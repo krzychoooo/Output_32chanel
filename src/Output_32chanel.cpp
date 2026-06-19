be89d5e82a21e5a195732ce5512a03c5cb8a2c40
krzychooo <krzysztofklimas@interia.pl>
2026-06-19 19:03:23 +0200

    edit class Output_32chanel

--- src/Output_32chanel.cpp
+++ src/Output_32chanel.cpp
@@ -8,5 +8,18 @@ Output_32chanel::~Output_32chanel(){
 }
 
 void Output_32chanel::setChanelValue(uint8_t chanel, bool value){
+    uint8_t hardwareChanel = this->mixerIndex[chanel];
+    if (value){
+        this->output32bitRegister |= 1 << chanel;
+    }
+    else{
+        this->output32bitRegister &= ~(1 << chanel);
+    }
+    if (hardwareChanel < 16){
+        chanel0x20.pin_write(hardwareChanel, value);
+    }
+    else{
+        chanel0x21.pin_write(hardwareChanel, value);
+    }
     
 }
